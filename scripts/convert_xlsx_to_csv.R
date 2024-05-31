e1462cc71c78c54cff67077bb77a6b0fb8f3f1db
Rich Yaxley <richard.yaxley@sagebase.org>
2024-05-31 13:13:59 -0700

    Only convert if files do not exist

--- scripts/convert_xlsx_to_csv.R
+++ scripts/convert_xlsx_to_csv.R
@@ -1,20 +1,21 @@
-# scripts/convert_xlsx_to_csv.R
+# convert_files.R
 
-# Load required libraries
 library(readxl)
 library(readr)
 library(stringr)
 
-# Function to convert xlsx to csv
 convert_xlsx_to_csv <- function(input_path) {
-  # Create the output path by replacing .xlsx or .xls with .csv
   output_path <- str_replace(input_path, "\\.xlsx?$", ".csv")
-
-  # Read the Excel file
   data <- read_excel(input_path)
-
-  # Write the data to a CSV file
   write_csv(data, output_path)
-
   message("Converted ", input_path, " to ", output_path)
+}
+
+convert_if_not_exists <- function(input_path) {
+  output_path <- str_replace(input_path, "\\.xlsx?$", ".csv")
+  if (!file.exists(output_path)) {
+    convert_xlsx_to_csv(input_path)
+  } else {
+    message("Output file already exists: ", output_path)
+  }
 }
