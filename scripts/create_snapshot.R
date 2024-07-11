5fb1d89a37e560b048cf2fa4edab8d2a37f8df4c
Rich Yaxley <richard.yaxley@sagebase.org>
2024-07-11 10:29:17 -0700

    Update create_snapshot.R

--- scripts/create_snapshot.R
+++ scripts/create_snapshot.R
@@ -1,43 +1,22 @@
-# Load the synapser package
+# version_label <- gsub("0([1-9])", "\\1", format(Sys.Date(), "%y.%m"))
+
 library(synapser)
+library(stringr)
 
-# Authenticate with Synapse
-synLogin()
+synLogin(silent = TRUE)
 
-# Define the Synapse ID of the File View
 file_view_id <- "syn11346063"
+# autolabel option version_label <- gsub("0([1-9])", "\\1", format(Sys.Date(), "%y.%m"))
+version_label <- "24.6"
+snapshot_comment <- paste(version_label, "data release")
 
-# Retrieve the current version number of the file view
+# Retrieve entity and create snapshot
 entity <- synGet(file_view_id)
-current_version <- entity$properties$versionNumber
-new_version <- current_version + 1
+cat("Creating snapshot for Synapse ID:", file_view_id, "with comment:", snapshot_comment, "\n")
 
-# Generate the snapshot comment based on the current year and month
-snapshot_comment <- format(Sys.Date(), "%y.%m data release")
-
-# Show what changes will be made
-cat("Creating a snapshot for Synapse ID:", file_view_id, "\n")
-cat("Current version:", current_version, "\n")
-cat("New version:", new_version, "\n")
-cat("Snapshot comment:", snapshot_comment, "\n")
-
-# Create the snapshot using synCreateSnapshotVersion
-snapshot <- synCreateSnapshotVersion(entity = file_view_id, snapshotComment = snapshot_comment)
-
-# Print the snapshot details
-print(snapshot)
-
-# Assign a DOI to the snapshot
-# Define DOI details
-doi_details <- list(
-  doi = FALSE,
-  doiTitle = snapshot_comment,
-  doiAuthors = "Sage Bionetworks",
-  doiType = "Collection"
-)
-
-# Get the newly created snapshot version ID
-snapshot_version <- snapshot$versionNumber
-
-# Print confirmation
-cat("Snapshot created with version", snapshot_version, "and comment:", snapshot_comment, "\n")
+if (tolower(readline(prompt = "Create snapshot? (yes/no): ")) == "yes") {
+  snapshot <- synCreateSnapshotVersion(table = file_view_id, comment = snapshot_comment, label = version_label, wait = TRUE)
+  cat("Snapshot created with version", snapshot$versionNumber, "\n")
+} else {
+  cat("Snapshot creation cancelled.\n")
+}
